5cdacff9e69fba01eb1a4b4227c14d368de0a000
Kuba Bartusiak <Kuba Bartusiak>
2016-03-19 19:45:29 +0100

    Implmeneted Array Container

--- src/com/ambiguous/containers/ArrayContainer.java
+++ src/com/ambiguous/containers/ArrayContainer.java
@@ -1,7 +1,45 @@
 package com.ambiguous.containers;
 
+import java.util.ArrayList;
+
 /**
  * Created by Kubuś on 2016-03-19.
  */
-public class ArrayContainer {
+public class ArrayContainer<T>{
+    private static final int INIT_SIZE=50;
+    private T[] m_array;
+    private int m_size;
+
+    public ArrayContainer(){
+        m_array = (T[])new Object[INIT_SIZE];
+        m_size = 0;
+    }
+
+    public T get(int index){
+        if(index>=m_size) throw new ArrayIndexOutOfBoundsException();
+        return m_array[index];
+    }
+
+    public int getSize(){
+        return m_size;
+    }
+
+    public void add(T item){
+        m_array[m_size]=item;
+        m_size++;
+    }
+
+    public void remove(int index){
+        if(index>=m_size) throw new ArrayIndexOutOfBoundsException();
+        ArrayContainer<T> temp = new ArrayContainer<>();
+        for (int i=0; i<m_size; i++){
+            if(i!=index-1) temp.add(m_array[i]);
+        }
+        m_size--;
+        m_array=temp.getArray();
+    }
+
+    private T[] getArray(){
+        return m_array;
+    }
 }
